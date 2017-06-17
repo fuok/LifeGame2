982531447739758dd6e50ac1d2e3492fa0c58215
LLL <beast_7th@hotmail.com>
2017-06-17 12:31:11 +0800

    nothing

--- Assets/_scripts/TreeLogic.cs
+++ Assets/_scripts/TreeLogic.cs
@@ -18,15 +18,42 @@ public class TreeLogic : MonoBehaviour
 
 	public Material mMatBlack, mMatGreen;
 
-//	void Start ()
-//	{
-//		
-//	}
+	//	void Start ()
+	//	{
+	//
+	//	}
 
-//	void Update ()
-//	{
-//		
-//	}
+	//	void Update ()
+	//	{
+	//
+	//	}
+
+	//获取当前树周围的8棵树对象，游戏开始时通过消息调用一次
+	private void GetAllTreesAround ()
+	{
+		StartCoroutine (GetAllTreesAroundCoroutine ());
+	}
+
+	IEnumerator GetAllTreesAroundCoroutine ()
+	{
+		//		int count = 0;
+		for (int i = 0; i < 8; i++) {
+			yield return new WaitForFixedUpdate ();
+			Ray ray = new Ray (transform.position, Quaternion.Euler (0f, 45f * i, 0f) * Vector3.forward);
+			RaycastHit hit;
+			Physics.Raycast (ray, out hit, 1f);
+			if (hit.collider) {
+//				print ("打到了：" + hit.collider.name);//ok
+				//				if (hit.collider.gameObject.GetComponent<TreeLogic> ().mCurrentStatus == TreeStatus.Alive) {
+				//					count++;
+				//				}
+				mAliveAroundList.Add (hit.collider.gameObject);
+			}
+		}
+		//		mAliveAround = count;
+	}
+
+	//---------------------PUBLIC-----------------------------------------------------------
 
 	public void TreeGrow ()
 	{
@@ -43,7 +70,6 @@ public class TreeLogic : MonoBehaviour
 	//获取当前树周围活着的树
 	public void GetAliveAround ()
 	{
-//		StartCoroutine (GetAliveAroundCoroutine ());
 		int count = 0;
 		for (int i = 0; i < mAliveAroundList.Count; i++) {
 			if (mAliveAroundList [i].GetComponent<TreeLogic> ().mCurrentStatus == TreeStatus.Alive) {
@@ -51,49 +77,6 @@ public class TreeLogic : MonoBehaviour
 			}
 		}
 		mAliveAround = count;
-	}
-
-	//	IEnumerator GetAliveAroundCoroutine ()
-	//	{
-	//		int count = 0;
-	//		for (int i = 0; i < 8; i++) {
-	//			yield return new WaitForFixedUpdate ();
-	//			Ray ray = new Ray (transform.position, Quaternion.Euler (0f, 45f * i, 0f) * Vector3.forward);
-	//			RaycastHit hit;
-	//			Physics.Raycast (ray, out hit, 1f);
-	//			if (hit.collider) {
-	//				print ("打到了：" + hit.collider.name);
-	//				if (hit.collider.gameObject.GetComponent<TreeLogic> ().mCurrentStatus == TreeStatus.Alive) {
-	//					count++;
-	//				}
-	//			}
-	//		}
-	//		mAliveAround = count;
-	//	}
-
-	//获取当前树周围的8棵树对象，游戏开始时调用一次
-	public void GetAllTreesAround ()
-	{
-		StartCoroutine (GetAllTreesAroundCoroutine ());
-	}
-
-	IEnumerator GetAllTreesAroundCoroutine ()
-	{
-//		int count = 0;
-		for (int i = 0; i < 8; i++) {
-			yield return new WaitForFixedUpdate ();
-			Ray ray = new Ray (transform.position, Quaternion.Euler (0f, 45f * i, 0f) * Vector3.forward);
-			RaycastHit hit;
-			Physics.Raycast (ray, out hit, 1f);
-			if (hit.collider) {
-				print ("打到了：" + hit.collider.name);
-//				if (hit.collider.gameObject.GetComponent<TreeLogic> ().mCurrentStatus == TreeStatus.Alive) {
-//					count++;
-//				}
-				mAliveAroundList.Add (hit.collider.gameObject);
-			}
-		}
-//		mAliveAround = count;
 	}
 
 	//更新树的状态
